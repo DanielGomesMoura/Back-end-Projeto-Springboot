6df90a28a901d0257b26e5ef60c16fe8f126701e
Daniel Moura <daniel_moura>
2023-03-28 10:30:25 -0400

    Implememtado a API de cursos e a conexção via rota

--- back-end/src/main/java/com/daniel/crudspring/CrudSpringApplication.java
+++ back-end/src/main/java/com/daniel/crudspring/CrudSpringApplication.java
@@ -1,7 +1,12 @@
 package com.daniel.crudspring;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.daniel.crudspring.model.Course;
+import com.daniel.crudspring.repository.CourseRepository;
 
 @SpringBootApplication
 public class CrudSpringApplication {
@@ -10,4 +15,15 @@ public class CrudSpringApplication {
 		SpringApplication.run(CrudSpringApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner initDataBase(CourseRepository courseRepository){
+		return args -> {
+			courseRepository.deleteAll();
+
+			Course c = new Course();
+			c.setName("frontbatata");
+			c.setCategory("Front-end");
+			courseRepository.save(c);
+		};
+	}
 }
