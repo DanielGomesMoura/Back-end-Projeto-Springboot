bccad05a4419bf75a738ed5f7ed5b6a423581d76
Daniel Moura <daniel.tecnicop@gmail.com>
2023-04-12 07:44:14 -0400

    implementado metodo criar

--- src/main/java/com/daniel/crudspring/controller/CourseController.java
+++ src/main/java/com/daniel/crudspring/controller/CourseController.java
@@ -2,8 +2,16 @@ package com.daniel.crudspring.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.crudspring.model.Course;
@@ -18,11 +26,19 @@ public class CourseController {
  
     private final CourseRepository courseRepository;    
 
+    //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
     }
 
-
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+    
+        return courseRepository.save(course); 
+        //ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+    }
 
 }
